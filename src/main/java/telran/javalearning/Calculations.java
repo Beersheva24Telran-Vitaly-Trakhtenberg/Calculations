4733f0e07b0285ad67d048f37d03116a14ed2d79
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-04 17:23:18 +0300

    Implemented missing methods and fixed tests

--- src/main/java/telran/javalearning/Calculations.java
+++ src/main/java/telran/javalearning/Calculations.java
@@ -1,4 +1,46 @@
 package telran.javalearning;
 
-public class Calculations {
+public class Calculations
+{
+    public static int sum(int arg1, int arg2)
+    {
+        return arg1 + arg2;
+    }
+    public static int dif(int arg1, int arg2)
+    {
+        return arg1 - arg2;
+    }
+    public static int mul(int arg1, int arg2)
+    {
+        return arg1 * arg2;
+    }
+    public static int sumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+    public static int maxDigit(int number)
+    {
+        int max = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            if (digit > max) {
+                max = digit;
+            }
+            number /= 10;
+        }
+        return max;
+    }
+    public static boolean isDividedOn(int source, int divider)
+    {
+        if (divider != 0) {
+            return source % divider == 0;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/telran/javalearning/CalculationsTest.java
+++ src/test/java/telran/javalearning/CalculationsTest.java
@@ -1,4 +1,51 @@
 package telran.javalearning;
 
-public class CalculationsTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static telran.javalearning.Calculations.*;
+
+public class CalculationsTest
+{
+    @Test
+    void sumTest()
+    {
+        assertEquals(4, sum(1, 3));
+    }
+
+    @Test
+    void difTest()
+    {
+        assertEquals(-2, dif(1, 3));
+        assertEquals(2, dif(3, 1));
+    }
+
+    @Test
+    void mulTest()
+    {
+        assertEquals(6, mul(2, 3));
+        assertEquals(6, mul(3, 2));
+    }
+
+    @Test
+    void sumOfDigitsTest()
+    {
+        assertEquals(15, sumOfDigits(1608));
+    }
+
+    @Test
+    void maxDigitTest()
+    {
+        assertEquals(8, maxDigit(1608));
+    }
+
+    @Test
+    void isDividedOnTest()
+    {
+        assertTrue(isDividedOn(14, 7));
+        assertFalse(isDividedOn(16, 7));
+        assertFalse(isDividedOn(16, 0));
+    }
+
 }
